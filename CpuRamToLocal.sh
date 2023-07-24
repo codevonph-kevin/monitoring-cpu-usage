b0bfe3d2b11cf94217fb72e7bf73512ced71f08e
codevonph-kevin <kevin@theteam247.com>
2023-07-24 17:54:12 +0800

    fix: saving to one file in one day

--- CpuRamToLocal.sh
+++ CpuRamToLocal.sh
@@ -8,7 +8,7 @@ mkdir -p "$LOG_DIR"
 # Get the current date and time to use in the log file name
 timestamp=$(date "+%Y%m%d%H%M%S")
 
-LOG_FILE="$LOG_DIR/system_status_$timestamp.log"
+LOG_FILE="$LOG_DIR/system_status_$(date '+%Y%m%d').log"
 
 cpuuse=$(sar -P ALL 1 2 | grep 'Average.*all' | awk -F" " '{print 100.0 -$NF}')
 memtotal=$(free -m | awk '/^Mem:/{print $2}')
@@ -29,4 +29,5 @@ if [ "$(echo "$cpuuse > 70" | bc -l)" -eq 1 ] || [ "$(echo "$memusepercent > 70"
     echo "Top 5 Processes consuming high Memory" >> $LOG_FILE
     echo "+------------------------------------------------------------------+" >> $LOG_FILE
     ps -eo pmem,pid,user,args | sort -k 1 -r | head -6 >> $LOG_FILE
+    echo "--------------------------------------------------------------------" >> $LOG_FILE
 fi
